0db756a2e9835f11868ea722685fd59f556a36c4
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-02-28 14:48:40 +0500

    displays the operator and the second number on the history

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const numberBtn = document.querySelectorAll(".num");
 const display = document.querySelector(".display");
+const resultText = document.querySelector(".result");
 const currentText = document.querySelector(".current");
+const operator = document.querySelectorAll(".opr");
 console.log(numberBtn);
-
+let number1 = 0;
+let number2 = 0;
+let beforeOperator = true;
 numberBtn.forEach((number) => {
     number.addEventListener("click",(e) => {
-        console.log(e.target.innerText);
         currentText.innerText += e.target.innerText;
+    })
+})
+
+operator.forEach((operator) => {
+    operator.addEventListener("click",(e) => {
+        if (beforeOperator) {
+            number1 = +e.target.innerText;
+            resultText.innerText = currentText.innerText + " " +  e.target.innerText + " ";
+            currentText.innerText = "";
+            beforeOperator = false;
+        }
+        else {
+                console.log("current text",currentText.innerText);
+                number2 = currentText.innerText;
+                resultText.innerText +=" "+currentText.innerText;
+        }
     })
 })
