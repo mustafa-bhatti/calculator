3d5ea2815d3f962cb3f051ee29272448ac11fbc1
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-03-01 03:01:23 +0500

    Refactor all the evenListener functions into generic function ready to be used with keyboard event listeners

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const numberBtn = document.querySelectorAll(".num");
 const display = document.querySelector(".display");
 const resultText = document.querySelector(".result");
 const currentText = document.querySelector(".current");
-const operator = document.querySelectorAll(".opr");
+const operatorBtn = document.querySelectorAll(".opr");
 const eq = document.querySelector(".eq");
 const delBtn = document.querySelector(".delete");
 const dotBtn = document.querySelector(".dot");
@@ -17,60 +17,56 @@ let tempNum = 0;
 let zeroCheck = false;
 let dotCheck = false;   
 
-numberBtn.forEach((number) => {
-    number.addEventListener("click",(e) => {
-        if (afterEval || zeroCheck) {
-            clear();
-        }
-        if (currentText.innerText.length<15){
-        currentText.innerText += e.target.innerText;
-        }
-    })
-})
 
-operator.forEach((operator) => {
-    operator.addEventListener("click",(e) => {
-        if (zeroCheck) {
-            clear();
-            return;
-        }
-        let onlyDotCheck = (currentText.innerText != ".");
-        if (beforeOperator && currentText.innerText.length > 0 && onlyDotCheck) {
-            number1 = currentText.innerText;
-            resultText.innerText = currentText.innerText + " " +  e.target.innerText + " ";
-            tempNum = number1;
-            currentText.innerText = "";
-            beforeOperator = false;
-            currentOp = e.target.innerText;
-            afterEval = false;
-            dotCheck = false;
-            // operator = e.target.innerText;
-            // console.log(operator);
-        }
-        else {  
-                if (currentOp == e.target.innerText && onlyDotCheck){
-                    if (!afterEval) {
-                        number2 = currentText.innerText;
-                        resultText.innerText +=" "+currentText.innerText;
-                        evaluate(+number1,+number2,currentOp)
-                        afterEval = true;
-                }   else {
-                        number1 = result;
-                        resultText.innerText = number1 + " " + currentOp + " ";
-                        currentText.innerText= "";
-                        afterEval = false;} 
-            }
-                else {
-                resultText.innerText = tempNum + " " +  e.target.innerText + " ";
-                currentOp = e.target.innerText;
+function addNumber(num) {
+    if (afterEval || zeroCheck) {
+        clear();
+    }
+    if (currentText.innerText.length<15){
+    currentText.innerText += num;
+    }
+}
 
-            }
+function addOperator(operator){
+    if (zeroCheck) {
+        clear();
+        return;
+    }
+    let onlyDotCheck = (currentText.innerText != ".");
+    if (beforeOperator && currentText.innerText.length > 0 && onlyDotCheck) {
+        number1 = currentText.innerText;
+        resultText.innerText = currentText.innerText + " " +  operator + " ";
+        tempNum = number1;
+        currentText.innerText = "";
+        beforeOperator = false;
+        currentOp = operator;
+        afterEval = false;
+        dotCheck = false;
+        // operator = operator;
+        // console.log(operator);
+    }
+    else {  
+            if (currentOp == operator && onlyDotCheck){
+                if (!afterEval) {
+                    number2 = currentText.innerText;
+                    resultText.innerText +=" "+currentText.innerText;
+                    evaluate(+number1,+number2,currentOp)
+                    afterEval = true;
+            }   else {
+                    number1 = result;
+                    resultText.innerText = number1 + " " + currentOp + " ";
+                    currentText.innerText= "";
+                    afterEval = false;} 
         }
-       if (e.target.innerText == "CE") {
-            clear();
-       }
-    })
-})
+            else {
+            resultText.innerText = tempNum + " " +  operator + " ";
+            currentOp = operator;
+
+        }
+    }
+   if (operator == "CE") {
+        clear();}
+}
 function clear() {
     number1 = 0;
     number2 = 0;
@@ -86,7 +82,6 @@ function clear() {
 
 }
 function evaluate(numA,numB,operator) {
-    console.log("Evaluate - ",numA,numB,operator);
     switch (operator) {
         case "+":
             result = numA + numB;
@@ -109,28 +104,51 @@ function evaluate(numA,numB,operator) {
        }
     beforeOperator = true;
     afterEval = true;
+    if (!Number.isInteger(result)) {
+        result = roundTo(result,15)
+    }
     currentText.innerText = result;
+
 }
-
-eq.addEventListener("click",() => {
-
+function roundTo(value, decimals) {
+    let rounded =  Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+    if (rounded.length > 15) {
+        return rounded.toExponential();
+    }
+    return rounded;
+}
+function evalOperation() {
     if (!beforeOperator && currentText.innerText.length > 0) {
         resultText.innerText +=" "+currentText.innerText;
         number2 = currentText.innerText;
         evaluate(+number1,+number2,currentOp);
     }
+}
 
-})
-
-delBtn.addEventListener("click",() => {
+function deleteLast(){
     if (currentText.innerText.length > 0) {
         currentText.innerText = currentText.innerText.substring(0,currentText.innerText.length-1)
     }
-})
+}
 
-dotBtn.addEventListener("click",() => {
+function addDot() {
     if(!dotCheck){
         dotCheck = true;
         currentText.innerText += ".";
     }
+}
+numberBtn.forEach((number) => {
+    number.addEventListener("click",(e) => {
+        addNumber(e.target.innerText);
+    })
 })
+
+operatorBtn.forEach((operatorB) => {
+    operatorB.addEventListener("click",(e)=> {
+        addOperator(e.target.innerText);
+    })    
+});
+
+eq.addEventListener("click", evalOperation);
+delBtn.addEventListener("click", deleteLast);
+dotBtn.addEventListener("click", addDot);
